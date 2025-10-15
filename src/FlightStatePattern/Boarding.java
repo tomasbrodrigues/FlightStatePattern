8f0465e6dc3cde709e641d6ca101a37486fe7fec
duarteayres2004 <duarte.v.ayres@gmail.com>
2025-10-15 18:26:53 +0100

    Added methods to all classes that are children of FlightState interface.

--- src/FlightStatePattern/Boarding.java
+++ src/FlightStatePattern/Boarding.java
@@ -1,4 +1,15 @@
 package FlightStatePattern;
 
 public class Boarding implements FlightState {
+
+
+    @Override
+    public void nextState(Flight flight) {
+
+    }
+
+    @Override
+    public void prevState(Flight flight) {
+
+    }
 }
